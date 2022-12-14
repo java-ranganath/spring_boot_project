e4d950c71fc7d4d03ee81c4d528ca76d6aa61c3f
java-ranganath <ranganath8696@gmail.com>
2022-12-14 23:24:00 +0530

    checking the commit is working or not

--- src/main/java/com/example/Laptop/Laptop.java
+++ src/main/java/com/example/Laptop/Laptop.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+adf
 @Entity 
 @Table(name="laps")
 public class Laptop {
